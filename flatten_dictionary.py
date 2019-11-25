b655c76632437b9ec063b7dcaf259384ed29f1f1
xb4dc0d3 <johnbakeryholland@gmail.com>
2019-11-25 18:38:40 +0700

    Add Flattern Dictionary [Not Solved]

---
+++ flatten_dictionary.py
@@ -0,0 +1,40 @@
+'''
+Hi, here's your problem today. This problem was recently asked by Google:
+
+Given a nested dictionary, flatten the dictionary, where nested dictionary keys can be represented through dot notation.
+
+Example:
+
+Input: {
+  'a': 1,
+  'b': {
+    'c': 2,
+    'd': {
+      'e': 3
+    }
+  }
+}
+Output: {
+  'a': 1,
+  'b.c': 2,
+  'b.d.e': 3
+}
+
+You can assume there will be no arrays, and all keys will be strings.
+'''
+
+
+def flatten_dictionary(d):
+    pass
+
+d = {
+    'a': 1,
+    'b': {
+        'c': 2,
+        'd': {
+            'e': 3
+        }
+    }
+}
+print(flatten_dictionary(d))
+# {'a': 1, 'b.c': 2, 'b.d.e': 3}
