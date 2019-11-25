6cd1624b8e74351df6a4e163e2622434a3a5afbc
xb4dc0d3 <johnbakeryholland@gmail.com>
2019-11-25 19:00:06 +0700

    Shortest Unique Prefix [Not Solved]

---
+++ shortest_unique_prefix.py
@@ -0,0 +1,20 @@
+'''
+Hi, here's your problem today. This problem was recently asked by Facebook:
+
+Given a list of words, for each word find the shortest unique prefix. 
+You can assume a word will not be a substring of another word (ie play and playing won't be in the same words list)
+
+Example
+
+Input: ['joma', 'john', 'jack', 'techlead']
+Output: ['jom', 'joh', 'ja', 't']
+
+'''
+
+
+def shortest_unique_prefix(words):
+    pass
+  
+  
+print(shortest_unique_prefix(['joma', 'john', 'jack', 'techlead']))
+# ['jom', 'joh', 'ja', 't']
