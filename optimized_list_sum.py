d6c1332318de694f95119d11e5e03bea71986c51
xb4dc0d3 <johnbakeryholland@gmail.com>
2019-12-02 22:23:56 +0700

    Add Optimized List Sum [Solved]

---
+++ optimized_list_sum.py
@@ -0,0 +1,22 @@
+'''
+[Daily Problem] Optimized List Sum
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Create a class that initializes with a list of numbers and has one method called sum. 
+sum should take in two parameters, start_idx and end_idx and return the sum of the list from start_idx (inclusive) to end_idx` (exclusive).
+
+You should optimize for the sum method.
+'''
+
+class ListFastSum:
+  def __init__(self, nums):
+    self.nums = nums
+
+  def sum(self, start_idx, end_idx):
+    total_sum = 0
+    for i in range(start_idx, end_idx):
+        total_sum += self.nums[i]
+    return total_sum
+
+print(ListFastSum([1, 2, 3, 4, 5, 6, 7]).sum(2, 5))
+# 12 because 3 + 4 + 5 = 12
